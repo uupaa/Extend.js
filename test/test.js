525624fb219abd057325caec1154af499b3639b3
uupaa <uupaa.js@gmail.com>
2014-03-18 23:48:57 +0900

    update settings.

--- test/test.js
+++ test/test.js
@@ -1,11 +1,15 @@
 new Test().add([
         testExtend,
-    ]).run().worker(function(err, test) {
-        if (!err && typeof Extend_ !== "undefined") {
-            var name = Test.swap(Extend, Extend_);
+    ]).run(function(err, test) {
+        if (1) {
+            err || test.worker(function(err, test) {
+                if (!err && typeof Extend_ !== "undefined") {
+                    var name = Test.swap(Extend, Extend_);
 
-            new Test(test).run(function(err, test) {
-                Test.undo(name);
+                    new Test(test).run(function(err, test) {
+                        Test.undo(name);
+                    });
+                }
             });
         }
     });
