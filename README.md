543631d96a83e8505e5d974d2e8c614b7f6b3a21
uupaa <uupaa.js@gmail.com>
2014-05-07 22:35:00 +0900

    follow Test.js

--- README.md
+++ README.md
@@ -8,9 +8,9 @@ Mixin, Polyfill, Fallback functions.
 
 # Document
 
-- [WebModule](https://github.com/uupaa/WebModule) ([Slide](http://uupaa.github.io/Slide/slide/WebModule/index.html))
-- [Development](https://github.com/uupaa/WebModule/wiki/Development)
 - [Extend.js wiki](https://github.com/uupaa/Extend.js/wiki/Extend)
+- [Development](https://github.com/uupaa/WebModule/wiki/Development)
+- [WebModule](https://github.com/uupaa/WebModule) ([Slide](http://uupaa.github.io/Slide/slide/WebModule/index.html))
 
 
 # How to use
--- test/test.js
+++ test/test.js
@@ -1,14 +1,12 @@
-var ModuleTest = (function(global) {
+var ModuleTestExtend = (function(global) {
 
-return new Test({
+return new Test("Extend", {
         disable:    false,
-        node:       true,
         browser:    true,
         worker:     true,
+        node:       true,
         button:     true,
         both:       true,
-        primary:    global["Extend"],
-        secondary:  global["Extend_"],
     }).add([
         testExtend,
         testExtend_override,
@@ -36,10 +34,8 @@ function testExtend(next) {
         baseObject.Object.freeze === Object.freeze &&
         baseObject.Array.isArray === Array.isArray) {
 
-        console.log("testExtend ok");
         next && next.pass();
     } else {
-        console.error("testExtend ng");
         next && next.miss();
     }
 }
@@ -76,10 +72,8 @@ function testExtend_override(next) {
         baseObject.Object.freeze === NOP2 &&
         baseObject.Array.isArray === NOP2) {
 
-        console.log("testExtend_override ok");
         next && next.pass();
     } else {
-        console.error("testExtend_override ng");
         next && next.miss();
     }
 }
@@ -107,10 +101,8 @@ function testExtend_define(next) {
         baseObject.freeze === Object.freeze &&
         baseObject.isArray === Array.isArray) {
 
-        console.log("testExtend_define ok");
         next && next.pass();
     } else {
-        console.error("testExtend_define ng");
         next && next.miss();
     }
 }
@@ -131,10 +123,8 @@ function testExtend_define_override(next) {
     if (Object.keys(baseObject).join() === "" && // enumerable false
         baseObject.keys === NOP) {
 
-        console.log("testExtend_define_override ok");
         next && next.pass();
     } else {
-        console.error("testExtend_define_override ng");
         next && next.miss();
     }
 }
@@ -163,10 +153,8 @@ function testExtend_mixin(next) {
         baseObject.b === 2 &&
         baseObject.c === 3) {
 
-        console.log("testExtend_mixin ok");
         next && next.pass();
     } else {
-        console.error("testExtend_mixin ng");
         next && next.miss();
     }
 }
